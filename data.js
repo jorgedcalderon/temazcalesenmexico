a4fdb5a8db4f1eac68411439b6ad1339990d74ab
David Calderon <calderon.djorge@gmail.com>
2018-12-13 03:11:24 +0000

    quitando data

--- data.js
+++ data.js
@@ -1,6 +1,10 @@
+//data
 var data = {
     mailKey: "key-b48534b7a46ccbceda643ad01b41f1a9",
-    mailUser: "mail.temazcal.info"
+    mailUser: "mail.temazcal.info",
+    cloudName: "davidcalderon",
+    cloudinaryKey: "656745249829433",
+    cloudinarySecret: "83UaLdwBj4cHAc34ODz6-KRsU4w"
 }
 
 module.exports = data;
